8d4ad23b1bc58db6b08f96a1124907c8a4df5bbb
maverick43 <maverick43@163.com>
2020-04-10 16:22:22 +0800

    微调界面

--- DuplicateFiles/DuplicateFiles/Form1.Designer.cs
+++ DuplicateFiles/DuplicateFiles/Form1.Designer.cs
@@ -215,7 +215,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 12F);
-            this.label2.Location = new System.Drawing.Point(317, 39);
+            this.label2.Location = new System.Drawing.Point(169, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 16);
             this.label2.TabIndex = 6;
